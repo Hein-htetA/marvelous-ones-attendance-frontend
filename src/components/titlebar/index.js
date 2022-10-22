f6639ed295ed6e6ea6613b10802eb9e3993fec47
Hein Htet <heinha123456@gmail.com>
2022-10-22 22:26:12 +0630

    initial setup

---
+++ src/components/titlebar/index.js
@@ -0,0 +1,5 @@
+export default function TitleBar() {
+    return (
+        <div>TitleBar</div>
+    )
+}
---
+++ src/data/students.js
@@ -0,0 +1,74 @@
+export const students = [
+    {
+        name: "Hein1",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein2",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein3",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein4",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein5",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein6",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein7",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+    {
+        name: "Hein8",
+        attendnece: {
+            week1: Array(7).fill(false),
+            week2: Array(7).fill(false),
+            week3: Array(7).fill(false),
+            week4: Array(7).fill(false)
+        }
+    },
+]
