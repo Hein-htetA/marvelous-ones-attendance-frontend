86ddc6217f1bb96eeaec42f7f742d1f60e252217
Hein Htet <heinha123456@gmail.com>
2022-10-30 20:06:01 +0630

    before restructuring batch

--- src/components/titlebar/BasicSelectBatch.js
+++ src/components/titlebar/BasicSelectBatch.js
@@ -1,44 +1,17 @@
 import * as React from 'react';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
-export default function BasicSelectBatch({ setStudents, batch, setBatch, setIsLoading }) {
+
+export default function BasicSelectBatch({ batch, setBatch }) {
 
   const handleChange = (event) => {
+
     setBatch(event.target.value);
   };
 
-  React.useEffect(() => {
-    console.log('fetching students')
-    async function fetchApi() {
-      setIsLoading(true);
-      try {
-        let response = await fetch(`http://localhost:5000/api/v1/students?batch=${batch}`);
-        response = await response.json();
-        setStudents(response.studentsByBatch)
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    }
-    fetchApi();
-  }, [batch, setStudents, setIsLoading])
-
   return (
-    <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
-        <Select
-          value={batch}
-          onChange={handleChange}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem value={1}>Batch I</MenuItem>
-          <MenuItem value={2}>Batch II</MenuItem>
-          <MenuItem value={3}>Batch III</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
+    <form>
+      <label htmlfor='batch'>batch</label>
+      <input type='number' value={batch} onChange={handleChange}></input>
+    </form>
   );
 }
--- src/components/titlebar/index.js
+++ src/components/titlebar/index.js
@@ -1,10 +1,27 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BasicSelectBatch from "./BasicSelectBatch";
 import BasicSelectWeek from './BasicSelectWeek';
 
 export default function TitleBar(props) {
     const [batch, setBatch] = useState(1);
+
+    useEffect(() => {
+        console.log('fetching students')
+        async function fetchApi() {
+          props.setIsLoading(true);
+          try {
+            let response = await fetch(`http://localhost:5000/api/v1/students?batch=${batch}`);
+            response = await response.json();
+            props.setStudents(response.studentsByBatch)
+          } catch (error) {
+            console.log(error);
+          }
+          props.setIsLoading(false);
+        }
+        fetchApi();
+      }, [])
+
     return (
         <Box
             display={'flex'}
